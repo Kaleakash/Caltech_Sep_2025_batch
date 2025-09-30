4f3c3c51376ec35992a869d6070235ec990c34e2
Akash <akash300383@gmail.com>
2025-09-30 07:42:52 +0530

    done

--- product-axios-rest-api/src/App.jsx
+++ product-axios-rest-api/src/App.jsx
@@ -7,6 +7,7 @@ let URL="http://localhost:3000/products";
 let [products,setProducts]=useState([]);
 
 let [product,setProduct]=useState({"pname":"",price:0.0});
+let [submitButtonValue,setSubmitButtonValue]=useState("Add Product");
 
 // load the data using ES5 style 
 // let loadProductData=()=> {
@@ -30,16 +31,31 @@ let loadProductData= async ()=> {
   }
 }
 
-let storeProduct=async (event)=> {
+let storeOrUpdateProduct=async (event)=> {
   event.preventDefault(); 
       console.log(product)
   // 1st parameter is url and 2nd parameter json data or js object 
+  if(submitButtonValue=="Add Product"){
+
+  
   try{
   let result = await axios.post(URL,product)
   console.log(result)
   }catch(error){
     console.log(error)
   }
+}else {
+    //alert("ready to update")
+    try{
+      // put method 1st parameter url with id must be appended and 2nd parameter json data. 
+      let result = await axios.put(URL+"/"+product.id,product);
+      console.log(result)
+      setSubmitButtonValue("Add Product")
+    }catch(error){
+      console.log(error)
+    }
+}
+
   setProduct({pname:"",price:0.0})
 }
 loadProductData();
@@ -54,18 +70,23 @@ let deleteProduct=async (id)=> {
     console.log(error)
   }
 }
+let readyToUpdateProduct=(product)=> {
+  //alert(product)
+  setProduct({"id":product.id,"pname":product.pname,"price":product.price})
+  setSubmitButtonValue("Update Product");
+}
   return (
     <>
     <h2>Product CRUD Operation using axios with json-server</h2>
     <h2>Store Product</h2>
-    <form onSubmit={storeProduct}>
+    <form onSubmit={storeOrUpdateProduct}>
       <label>PName</label>
       <input type='text' name='pname' value={product.pname}
       onChange={(event)=> setProduct({...product,"pname":event.target.value})} /> <br/>
       <label>Price</label>
       <input type='number' name='price' value={product.price}
       onChange={(event)=> setProduct({...product,"price":event.target.value})} /> <br/>
-      <input type='submit' value="Store Product"/>
+      <input type='submit' value={submitButtonValue}/>
       <input type='reset' value="reset"/>
     </form>
 
@@ -76,7 +97,8 @@ let deleteProduct=async (id)=> {
           <th>PId</th>
           <th>PName</th>
           <th>Price</th> 
-          <th>Delete</th> 
+          <th>Delete</th>
+          <th>Update</th> 
         </tr>      
     </thead>
     <tbody>
@@ -88,6 +110,9 @@ let deleteProduct=async (id)=> {
             <td>{p.price}</td>
             <td>
               <input type='button' value="delete" onClick={()=>deleteProduct(p.id)}/>
+            </td>
+            <td>
+              <input type='button' value="update" onClick={()=>readyToUpdateProduct(p)}/>
             </td>
           </tr>
         )
