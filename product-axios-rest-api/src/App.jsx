4ed9d027ec042a4c7ebd4a7b5241c9d7251d92f7
Akash <akash300383@gmail.com>
2025-09-30 06:41:47 +0530

    done

--- product-axios-rest-api/src/App.jsx
+++ product-axios-rest-api/src/App.jsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 function App() {
 let [products,setProducts]=useState([]);
 
+let [product,setProduct]=useState({"pname":"",price:0.0});
+
 // load the data using ES5 style 
 // let loadProductData=()=> {
 //   axios.get("http://localhost:3000/products").then(result=> {
@@ -26,11 +28,34 @@ let loadProductData= async ()=> {
     console.log(error)
   }
 }
-loadProductData();
 
+let storeProduct=async (event)=> {
+  event.preventDefault(); 
+      console.log(product)
+  // 1st parameter is url and 2nd parameter json data or js object 
+  try{
+  let result = await axios.post("http://localhost:3000/products",product)
+  console.log(result)
+  }catch(error){
+    console.log(error)
+  }
+  setProduct({pname:"",price:0.0})
+}
+loadProductData();
   return (
     <>
     <h2>Product CRUD Operation using axios with json-server</h2>
+    <h2>Store Product</h2>
+    <form onSubmit={storeProduct}>
+      <label>PName</label>
+      <input type='text' name='pname' value={product.pname}
+      onChange={(event)=> setProduct({...product,"pname":event.target.value})} /> <br/>
+      <label>Price</label>
+      <input type='number' name='price' value={product.price}
+      onChange={(event)=> setProduct({...product,"price":event.target.value})} /> <br/>
+      <input type='submit' value="Store Product"/>
+      <input type='reset' value="reset"/>
+    </form>
 
     <h3>All Product Details</h3>
     <table border="1">
