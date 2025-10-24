85dffe36bb733d54d3cfbb2858ad55fc58547d2f
Akash <akash300383@gmail.com>
2025-10-24 05:52:52 +0530

    done

--- Phase 4 - FSD - Data Structures and Algorithms/Programs/Reference data types/non linear data structure/SimpleTreeExample.js
+++ Phase 4 - FSD - Data Structures and Algorithms/Programs/Reference data types/non linear data structure/SimpleTreeExample.js
@@ -6,6 +6,25 @@ class TreeNode {
     addChild(childNode){
             this.children.push(childNode);
     }
+    // Additional methods can be added here as needed
+    // write function to travels the tree and prints each node's data using DFS
+    printTreeDFS(node){
+        console.log(node.data);         // Root - A
+        for(let child of node.children){
+            child.printTreeDFS(child);
+        }
+    } 
+    // write function to travels the tree and prints each node's data using BFS
+    printTreeBFS(node){
+        let queue = [node];     // Initialize queue with root node
+        while(queue.length > 0){
+            let currentNode = queue.shift();
+            console.log(currentNode.data);
+            for(let child of currentNode.children){
+                queue.push(child);
+            }
+        }
+    }
 }
 let root = new TreeNode("Root - A");
 let child1 = new TreeNode("Child - B");
@@ -21,10 +40,15 @@ child1.addChild(grandChild1);
 child1.addChild(grandChild2);
 child2.addChild(grandChild3);
 child2.addChild(grandChild4);
-console.log(root);
-console.log(child1);
-console.log(child2);
-console.log(grandChild1);
-console.log(grandChild2);
-console.log(grandChild3);
-console.log(grandChild4);
+
+root.printTreeDFS(root);
+console.log("----------------");
+root.printTreeBFS(root);
+
+// console.log(root);
+// console.log(child1);
+// console.log(child2);
+// console.log(grandChild1);
+// console.log(grandChild2);
+// console.log(grandChild3);
+// console.log(grandChild4);
