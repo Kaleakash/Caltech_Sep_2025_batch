9bb4787921986e90cf09764a90770d41178046b4
Akash <akash300383@gmail.com>
2025-11-18 06:25:59 +0530

    done

---
+++ Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/Basic Node JS Programs/employeeDetails.js
@@ -0,0 +1,12 @@
+let id=100;
+let fname ="John";
+let age = 21;
+let isActive = true;
+console.log("id is "+id)
+console.log("name is "+fname)
+console.log("age is "+age)
+console.log("isActive is "+isActive)
+function greeting() {
+    console.log("user defined function as greeting")
+}
+greeting();
