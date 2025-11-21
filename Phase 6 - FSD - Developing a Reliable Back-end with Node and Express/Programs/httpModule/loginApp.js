b3a6d2435c63d90b40693b66d76cafd3ae78784f
Akash <akash300383@gmail.com>
2025-11-21 07:14:03 +0530

    done

--- Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/httpModule/loginApp.js
+++ Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/httpModule/loginApp.js
@@ -8,6 +8,9 @@ let server = http.createServer((request,response)=> {
     if(urlRef.pathname=="/login"){
         let loginData = fs.readFileSync("login.html");
         response.write(loginData.toString());
+    }else if(urlRef.pathname=="/signUp"){
+        let loginData = fs.readFileSync("signUp.html");
+        response.write(loginData.toString());
     }else if(urlRef.pathname=="/checkLoginDetails" && request.method=="GET"){
         let loginData = urlRef.query
         // if(loginData.emailId=="admin@gmail.com" && loginData.password=="admin@123"){
@@ -20,8 +23,22 @@ let server = http.createServer((request,response)=> {
         let result = loginsDataFromFile.find(ll=>ll.emailId==loginData.emailId && ll.password==loginData.password)
         if(result != undefined){
             response.write("<h2>Successfully login done by "+loginData.emailId+"</h2>")
+            response.write("<a href='/'>Logout</a>")
         }else {
             response.write("<h2>Failure try once again</h2>")
+        }
+    }else if (urlRef.pathname=="/signUpDetails" && request.method=="GET") {
+        let loginData = urlRef.query
+        loginsDataFromFile = JSON.parse(fs.readFileSync("logins.json").toString());
+        let result = loginsDataFromFile.find(ll=>ll.emailId==loginData.emailId)
+        if(result != undefined){
+            response.write("<h2>Account didn't create EmailId already exists, "+loginData.emailId+"</h2>")
+            response.write("<a href='signUp'>SignUp</a>")
+        }else {
+            loginsDataFromFile.push(loginData);
+            fs.writeFileSync("logins.json",JSON.stringify(loginsDataFromFile));
+            response.write("<h2>Account created successfully</h2>")
+            response.write("<a href='login'>Login Page</a>")
         }
     }else if(urlRef.pathname=="/"){
         response.write("<h2>Welcome to Simple Http Module App!</h2>")
