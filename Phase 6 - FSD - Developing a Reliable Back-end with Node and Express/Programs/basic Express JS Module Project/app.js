888087249fa9e3bea415018fdbd0f804bb7290b3
Akash <akash300383@gmail.com>
2025-11-21 08:44:41 +0530

    done

--- Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/basic Express JS Module Project/app.js
+++ Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/basic Express JS Module Project/app.js
@@ -13,6 +13,22 @@ app.get("/contactUs",(request,response)=> {
     response.send("Welcome to Contact Us page")
 })
 
+app.get("/login",(request,response)=> {
+    //response.sendFile("login.html");
+   // response.sendFile("D:\\Desktop\\FSD - Designing a Dynamic Front-end with React Sep 2025\\Phase 6 - FSD - Developing a Reliable Back-end with Node and Express\\Programs\\basic Express JS Module Project\\login.html");
+    //response.send(__dirname);       // current directory path 
+    response.sendFile(__dirname+"//login.html");
+})
+
+app.get("/chechLoginDetails",(request,response)=> {
+    let emailId = request.query.emailId;
+    let password = request.query.password
+    if(emailId=="admin@gmail.com" && password=="123"){
+        response.send("Successfully login")
+    }else {
+        response.send("Failure try once again")
+    }
+})
 
 
 app.listen(9090,()=>console.log("Server up on port number 9090"))
