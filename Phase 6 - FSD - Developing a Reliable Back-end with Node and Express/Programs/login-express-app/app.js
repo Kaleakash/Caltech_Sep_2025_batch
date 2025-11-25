288af99915dad5042c556e16d9baa7817b90c904
Akash <akash300383@gmail.com>
2025-11-25 06:50:59 +0530

    done

--- Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/login-express-app/app.js
+++ Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/login-express-app/app.js
@@ -6,6 +6,11 @@ app.use(express.urlencoded({extended:true}));  // to parse application/x-www-for
 
 // http://localhost:3000/
 
+let loginDb= [
+    {"username":"john@gmail.com","password":"john@123"},
+    {"username":"raj@gmail.com","password":"raj@123"}
+]
+
 app.get("/",(request,response)=> {
 
     response.sendFile(__dirname + "/login.html");
@@ -20,18 +25,30 @@ app.post("/signIn",(request,response)=> {
     //if method is get we can receive the value using request.query.fieldname
     let login = request.body;
     console.log(login);
-    if(login.username==="admin@gmail.com" && login.password==="admin123"){
+    // if(login.username==="admin@gmail.com" && login.password==="admin123"){
+    //     response.sendFile(__dirname + "/success.html");
+    // }else {
+    //     response.sendFile(__dirname + "/failure.html");
+    // }
+    // testing with db (in memory array or db)
+    let userFound = loginDb.find( (user) => user.username === login.username && user.password === login.password );
+    if(userFound){
         response.sendFile(__dirname + "/success.html");
     }else {
         response.sendFile(__dirname + "/failure.html");
     }
-    
 })
 
 
 app.post("/signUp",(request,response)=> {
-
-    response.send("SigUp done Successful");
+    let signUpData = request.body;
+        let userFound = loginDb.find( (user) => user.username === signUpData.username);
+    if(userFound){
+        response.send("User already exists, please login");
+    }else {
+        loginDb.push(signUpData);
+        response.send("SignUp done Successful");
+    }
 })
 
 
