9241559b580102ad6b3ac6dd1d136ab8362b82a1
Akash <akash300383@gmail.com>
2025-11-25 08:48:27 +0530

    done

--- Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/rest-api-message-format/app.js
+++ Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/rest-api-message-format/app.js
@@ -1,6 +1,11 @@
 let express = require("express")
 let app = express()
-
+let user = {name:"John Doe", age:30, city:"New York"}
+let users=[
+    {name:"John Doe", age:30, city:"New York"},
+    {name:"Jane Smith", age:25, city:"Los Angeles"},
+    {name:"Mike Johnson", age:35, city:"Chicago"}
+]
 let message = "<abc>Welcome to the Express.js application with REST API</abc>"
 
 app.get("/text",(request,response)=> {
@@ -21,4 +26,31 @@ app.get("/json",(request,response)=> {
     response.type("text/json")
     response.send({"message":"Welcome to Express JS application with rest api - JSON message"})
 })
+
+app.get("/user",(request,response)=> {
+    response.type("application/json")
+    response.send(user)  // Sending user object as JSON
+})
+
+
+app.get("/users",(request,response)=> {
+   response.json(users)  // Sending array of user objects as JSON   
+})
+
+// http://localhost:3000/userQuery?name=John&age=21
+// query param 
+app.get("/userQuery",(request,response)=> {
+    let name = request.query.name
+    let age = request.query.age
+    response.send("Hello user "+name+" your age is "+age)
+})
+
+// http://localhost:3000/userPath/John/21
+// path param 
+app.get("/userPath/:name/:age",(request,response)=> {
+    let name = request.params.name
+    let age = request.params.age
+    response.send("Hello user "+name+" your age is "+age)
+})
+
 app.listen(3000, ()=>console.log("Server running on http://localhost:3000"))
