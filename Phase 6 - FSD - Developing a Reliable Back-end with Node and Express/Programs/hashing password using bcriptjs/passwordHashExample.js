4a390979e34fa57f2959a260504a6806752120f3
Akash <akash300383@gmail.com>
2025-12-12 06:27:09 +0530

    done

---
+++ Phase 6 - FSD - Developing a Reliable Back-end with Node and Express/Programs/hashing password using bcriptjs/passwordHashExample.js
@@ -0,0 +1,26 @@
+let bcrptjs = require("bcryptjs");
+let originalPassword = "admin@123";
+
+async function hashPassword(password){
+        let saltRound = 10;
+    let hashFormatPassword = await bcrptjs.hash(password,saltRound);
+    return hashFormatPassword;
+}
+async function displayPasswordDetails(){
+let hashFormatPassword = await hashPassword(originalPassword)
+console.log("original password "+originalPassword)
+console.log("hashFormat Password "+hashFormatPassword);
+compareHashPassword(originalPassword,hashFormatPassword)
+compareHashPassword("admin@123",hashFormatPassword);
+}
+
+async function compareHashPassword(originalPassword,hashPassword){
+        let match = await bcrptjs.compare(originalPassword,hashPassword);
+        if(match){
+            console.log("password match")
+        }else {
+            console.log("password mismatch")
+        }
+}
+
+displayPasswordDetails();
